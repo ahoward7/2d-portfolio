9acf5eea3fdd31a53079f1a58e13d6fed6e2c3bd
Avery Howard <avery.d.howard@gmail.com>
2025-08-30 13:07:13 -0400

    Remove gap again

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col gap-1 min-h-screen text-gray-500 font-code p-2 lg:p-4">
+  <div class="min-h-screen text-gray-500 font-code p-2 lg:p-4">
     <Header />
     <Intro />
     <Skills />
